fc3d8ae535390ed14da507d668bfd70f55cb0f6a
Sneyder-dev <sney11gomez@gmail.com>
2026-08-21 14:21:24 -0400

    API endpoints created

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,21 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    const documents = await Dealerships.find();
+    res.json(documents);
+
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    const documents = await Dealerships.find({ id: req.params.id });
+    res.json(documents);
 });
 
 //Express route to insert review
